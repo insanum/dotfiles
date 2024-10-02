f2434c0cea3a3dfe67e5b55b85ef648598f6941a
Eric Davis <edavis@insanum.com>
2024-10-02 13:02:51 -0700

    added new mark-signs neovim plugin I wrote

---
+++ config/nvim/lua/plugins/mark-signs.lua
@@ -0,0 +1,17 @@
+return {
+    -- dir = '/Volumes/work/git/mark-signs.nvim',
+    -- config = function()
+    --     require('mark-signs').setup {
+    --         builtin_marks = { '.', '^', '`', '\'', '"', '<', '>', '[', ']' },
+    --         sign_priority = { lower=10, upper=15, builtin=8 },
+    --     }
+    -- end,
+    'insanum/mark-signs.nvim',
+    event = 'VeryLazy',
+    config = function()
+        require('mark-signs').setup {
+            builtin_marks = { '.', '^', '`', '\'', '"', '<', '>', '[', ']' },
+            sign_priority = { lower=10, upper=15, builtin=8 },
+        }
+    end,
+}
--- config/nvim/lua/unused_plugs.lua
+++ config/nvim/lua/unused_plugs.lua
@@ -2,6 +2,17 @@
 return {
 
   {
+    'chentoast/marks.nvim',
+    event = 'VeryLazy',
+    config = function()
+        require('marks').setup {
+            builtin_marks = { '.', '<', '>', '^' },
+            sign_priority = { lower=10, upper=15, builtin=8, bookmark=20 },
+        }
+    end,
+  },
+
+  {
     'tpope/vim-fugitive',
     enabled = false,
     event = 'VeryLazy',
