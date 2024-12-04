663f6a79c968eace94153fbf7ecb2b0869a7b79d
Eric Davis <edavis@insanum.com>
2024-12-04 12:14:02 -0800

    neovim disable indentexpr to enable cinoptions

--- config/nvim/lua/keymaps.lua
+++ config/nvim/lua/keymaps.lua
@@ -10,6 +10,7 @@ vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufReadPost' }, {
     pattern = { '*.c', '*.h', '*.cc', '*.cpp', '*.ino', '*.cs', '*.java', '*.js', '*.lua', '*.rs' },
     callback = function()
         vim.opt.cindent = true
+        vim.opt.indentexpr = nil
         vim.opt.cinoptions = 's,e0,n0,f0,{0,}0,^0,:0,=s,gs,hs,ps,t0,+s,c1,(0,us,)20,*30,Ws'
     end
 })
